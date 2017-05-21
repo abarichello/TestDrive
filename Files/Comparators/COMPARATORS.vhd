c183f4cbc1a19eb46263e623f66be11e24115077
aBarichello <arturbarichello@hotmail.com>
2017-05-20 22:16:54 -0300

    Points.

--- Files/Comparators/COMPARATORS.vhd
+++ Files/Comparators/COMPARATORS.vhd
@@ -5,7 +5,9 @@ use ieee.std_logic_unsigned.all;
 entity COMPARATORS is
 	port (
 		CNT_U,CNT_D: in std_logic_vector(9 downto 0);
-		CNT_B: 		 in std_logic_vector(4 downto 0)
+		CNT_B: 		 in std_logic_vector(4 downto 0);
+		END_BONUS,END_TIME,TARGET: out std_logic;
+		POINT: out std_logic
 	);
 end COMPARATORS;
 
