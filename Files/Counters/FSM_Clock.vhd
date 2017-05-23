ba036460fd5c7089117b07adcd521c7fd4b98991
aBarichello <arturbarichello@hotmail.com>
2017-05-22 22:41:06 -0300

    Added FSM_Control

--- Files/Counters/FSM_Clock.vhd
+++ Files/Counters/FSM_Clock.vhd
@@ -14,7 +14,7 @@ architecture bhv of FSM_Clock is
 		signal EA,PE: STATES;
 		signal C50: std_logic;
 		signal contador: std_logic_vector(27 downto 0);
-		--TODO finish this.
+	--	TODO finish this.
 	begin
 		P1:process(CLOCK_50)
 		begin
@@ -39,20 +39,20 @@ architecture bhv of FSM_Clock is
 		begin
 			case EA is
 				when E0 =>
-					contagem <= "0001";
-					PE <= E1;
+				--	contagem <= "0001";
+				--	PE <= E1;
 				when E1 =>
-					contagem <= "0010";
-					PE <= E2;
+				--	contagem <= "0010";
+				--	PE <= E2;
 				when E2 =>
-					contagem <= "0011";
-					PE <= E3;
+				--	contagem <= "0011";
+				--	PE <= E3;
 				when E3 =>
-					contagem <= "0100";
-					PE <= E4;
+				--	contagem <= "0100";
+				--	PE <= E4;
 				when E4 =>
-					contagem <= "0101";
-					PE <= E0;
+				--	contagem <= "0101";
+				--	PE <= E0;
 			end case;
-		end process;
+		end process; 
 end bhv;
